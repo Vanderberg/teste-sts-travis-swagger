99e08022f0b334363f2f95e9867dca7fceb195ac
VanderNunesBR <tec.vander.nunes@gmail.com>
2019-07-28 20:21:52 -0300

    Adicionado projeto

---
+++ src/main/java/com/vandernunes/api/TesteStsTravisSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.vandernunes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteStsTravisSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteStsTravisSwaggerApplication.class, args);
+	}
+
+}
